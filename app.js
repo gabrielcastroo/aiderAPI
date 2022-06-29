480e26f921b04978487d140501c763f7e2acedf1
Gabriel Castro <gabrielcastrog2@gmail.com>
2022-06-28 23:04:14 -0300

    create user on database

--- app.js
+++ app.js
@@ -3,10 +3,14 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const User = require('./models/User')
 
 
 const app = express()
 
+// Config Json response
+
+app.use(express.json())
 
 // App Route - Public Route
 app.get('/', (req, res) => {
@@ -22,7 +26,45 @@ app.post('/auth/register', async(req,res) => {
     if(!name){
         return res.status(422).json({ msg: 'O nome é obrigatório'})
     }
+    if(!email){
+        return res.status(422).json({ msg: 'O Email é obrigatório'})
+    }
+    if(!password){
+        return res.status(422).json({ msg: 'A senha é obrigatória'})
+    }
+    if(password!=confirmpassword){
+        return res.status(422).json({ msg: 'As senhas informadas não conferem'})
+    }
 
+    // check if user exists
+    const userExists = await User.findOne({ email: email })
+
+    if(userExists){
+        return res.status(422).json({ msg: 'O email já está cadastrado'})
+    }
+
+    // create password
+    const salt = await bcrypt.genSalt(12)
+    const passwordHash = await bcrypt.hash(password, salt)
+
+    // create user
+    const user = new User({
+        name,
+        email,
+        password,
+    })
+
+    try{
+        await user.save()
+
+        res.status(201).json({msg: 'Usuário criado com sucesso!'})
+    } catch(error) {
+
+        console.log(error)
+
+        res.status(500).json({msg: error})
+    }
+   
 })
 
 // Credenciais
